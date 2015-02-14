ce95d7588641856d1d1eb26a9131cd46c5134594
Andrew <akv9509@saintjoe.edu>
2015-02-14 15:16:25 -0500

    This is a filled readme and a filled list of 25 players

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # dataset
 CMP237 Andrew Klump Data Set poject
+
+
+This data set is designed to hold information on people who have played basketball at Kentucky the last 10 years who also played in the NBA. It will have various facts about them, current NBA team, average points, year the left Kentucky, Whether they left early or graduated, etc...
---
+++ ukplayers.js
@@ -0,0 +1,8 @@
+//Andrew Klump
+//CMP237
+//February 2015
+//This is a data set of basketball players who played at Kentucky and played in the NBA from the past 15 years - only including players who have been drafted
+
+
+
+players = { "James Young": ['1','2','3','4','5','6'], "Julius Randle": ['2','2','3','4','5','6'], "Archie Goodwin": ['3','2','3','4','5','6'], "Nerlens Noel": ['4','2','3','4','5','6'], "Darius Miler":['5','2','3','4','5','6'], "Doron Lamb":['6','2','3','4','5','6'], "Marquis Teague":['7','2','3','4','5','6'], "Terrence Jones":['8','2','3','4','5','6'], "Michael Kidd-Gilchrist":['9','2','3','4','5','6'], "AnthonyDavis":['10','2','3','4','5','6'], "DeAndre Liggins":['11','2','3','4','5','6'], "Josh Harrellson":['12','2','3','4','5','6'], "Brandon Knight":['13','2','3','4','5','6'], "Enes Kanter":['14','2','3','4','5','6'], "Daniel Orton":['15','2','3','4','5','6'], "Eric Bledsoe":['16','2','3','4','5','6'], "Patrick Patterson":['17','2','3','4','5','6'], "Demarcus Cousins":['18','2','3','4','5','6'], "John Wall":['19','2','3','4','5','6'], "Jodie Meeks":['20','2','3','4','5','6'], "Joe Crawford":['21','2','3','4','5','6'], "Rajon Rondo":['22','2','3','4','5','6'], "Keith Bogans":['23','2','3','4','5','6'], "Tayshaun Prince":['24','2','3','4','5','6'], "Michael Bradley":['25','2','3','4','5','6']}
