daa0092d067b68f4a016c3531e7abe89c3a4f96d
Andrew <akv9509@saintjoe.edu>
2015-02-27 11:09:25 -0500

    New attempt at using the map, filter, and reduce method on my dataset -- unsuccessfull

--- testnba.js
+++ testnba.js
@@ -5,11 +5,41 @@
 
 load('nbauk.js');
 
+//used to reduce the aray to the number of players who played past 2010
 function calPlayers(name, year){
 	if(year >= 2010){
-		return Name + Year;
+		return name;
 	}
 }
 
+//used to filter to only the players who played point guard
+function getpg(name, position){
+	if(position === 'pg'){
+		return name;
+	}
+
+}
+
+//returns the name of the team who drafted the player based on who the user wants to see
+function getteam(name, team, usrteam){
+	if(team === usrteam){
+		return name;
+	}
+
+}
+
+
+
+
+//print number of players past 2010
 print(players.reduce(calPlayers(UK)));
+
+//print list of pointguards
+print(players.filter(getpg(UK)));
+
+
+//print new array of specific teams
+print(players.filter(getteam(UK)));
+
+
 
