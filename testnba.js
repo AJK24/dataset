0dac90c72d360578b5c2372228777a0db9c0cad1
Andrew <akv9509@saintjoe.edu>
2015-02-26 23:00:23 -0500

    First try at the reduce method

---
+++ testnba.js
@@ -0,0 +1,15 @@
+//Andrew Klump
+//CMP 237
+//February 2015
+//Testing the nbauk.js file with reduce map and filter
+
+load('nbauk.js');
+
+function calPlayers(name, year){
+	if(year >= 2010){
+		return Name + Year;
+	}
+}
+
+print(players.reduce(calPlayers(UK)));
+
