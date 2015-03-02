92d7e5f49f057c529f10b5a7f79a5d6d78893035
Andrew <akv9509@saintjoe.edu>
2015-03-01 20:36:45 -0500

    Added filter in testnba Have filter working, just not limiting to only names, saying all information about players of a certain position

--- testnba.js
+++ testnba.js
@@ -6,6 +6,22 @@
 load('nbauk.js');
 var players = JSON.parse(UK);
 
+
+//filter 
+function filter(array, test) {
+	  var passed = [];
+	    for (var i = 0; i < array.length; i++) {
+		        if (test(array[i]))
+				      passed.push(array[i]);
+			  }
+	      return passed;
+}
+
+
+
+
+
+
 //used to reduce the array to the number of players who played past 2010
 function calPlayers(){
 	if(players.Year >= 2010){
@@ -14,10 +30,12 @@ function calPlayers(){
 }
 
 //used to filter to only the players who played point guard
-function getpg(name, position){
-	if(players.Position === 'PG'){
-		return players.Name;
-	}
+function getpg(){
+///	if(players.Position === "PG"){
+//		return players.Name;
+//	}
+	return players.Position == "PG";
+
 
 }
 
@@ -44,8 +62,11 @@ function getteam(name, team, usrteam){
 print(players.reduce(calPlayers));
 
 //print list of pointguards
-print(players.filter(getpg));
-
+print(JSON.stringify(filter(players, function(item){
+	if(item.Position == "PG"){
+		return item.Name;
+	}
+}))); 
 
 //print new array of players who played for that team.
 //print(players.map(getteam("Raptors")));
